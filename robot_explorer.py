ec0e405ba4d7cc4c5a5cdfb899bc7d6f530f6239
umang <umang.rastogi96@gmail.com>
2020-06-04 17:31:14 -0400

    [robot_explorer.py] Use explorer class

--- robot_explorer.py
+++ robot_explorer.py
@@ -4,6 +4,7 @@ from sys import argv
 from time import time
 # Import necessary custom-built classes and methods
 from utils.obstacle_space import Map
+from utils.explorer import Explorer, check_node_validity
 
 
 """
@@ -25,5 +26,25 @@ if __name__ == '__main__':
     goal_node_coords = goal_node_coords[1], goal_node_coords[0]
     # Initialize the map class
     obstacle_map = Map(int(robot_radius), int(clearance))
+    # Initialize the explorer class
+    explorer = Explorer(start_node_coords, goal_node_coords, str(method))
     # Obstacle checking image
     check_image = obstacle_map.check_img.copy()
+    # Check validity of start and goal nodes
+    if not (check_node_validity(check_image, start_node_coords[1], obstacle_map.height - start_node_coords[0])
+            and check_node_validity(check_image, goal_node_coords[1], obstacle_map.height - goal_node_coords[0])):
+        print('One of the points lie in obstacle space!!\nPlease try again')
+        quit()
+    # Get start time for exploration
+    start_time = time()
+    # Start exploration
+    explorer.explore(check_image)
+    # Show time for exploration
+    print('Exploration Time:', time() - start_time)
+    if int(animation) == 1:
+        # Get start time for animation
+        start_time = time()
+        # Display animation of map exploration to find goal
+        explorer.show_exploration(obstacle_map.obstacle_img)
+        # Show time for animation
+        print('Animation Time:', time() - start_time)
